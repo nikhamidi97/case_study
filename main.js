d03bec3d13c01ed087669d7c97df74a8048ccefa
Nik Hamidi <hamidi.hamat@live.iium.edu.my>
2020-08-14 02:50:35 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const readline = require('readline');
+const { setTimeout } = require('timers');
+// import {Customer} from './activity2_student.mjs';
+
+const rl = readline.createInterface({
+
+input: process.stdin,
+
+output: process.stdout
+
+});
+let i=0;
+let queue = true;
+let qtime = [1000,2000,3000,4000];
+let qtimeran = qtime[Math.floor(Math.random()* qtime.length)];
+let custName = ["ali","abu","ahmad","syamil"]
+
+let counter = ["O","0","O","O","O"] ;
+
+
+           while(queue){
+                
+                
+                if(counter[i]=="O"){
+                    
+                    counter[i]=custName[Math.floor(Math.random()* custName.length)];
+                    console.log(counter);
+               
+                }
+                else{
+    
+                    counter[i]="O";
+                  setTimeout(()=>{  
+                    console.log(counter);
+                   
+                },1000000);
+                };
+
+                if (i==5) i = 0;
+                else i++;
+               
+                
+             };
+      
